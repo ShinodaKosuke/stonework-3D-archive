5a9f1ca93f01a49e9dbd2bbfafdfcbb1d08cba14
Midori IT Office, LLC <13025500+midoriit@users.noreply.github.com>
2019-11-13 17:19:32 +0900

    target="_blank"

--- csv2geojson.php
+++ csv2geojson.php
@@ -38,7 +38,7 @@
         } else if ( $types[$i] == "URL" ) {
           $properties = $properties + array($header[$i] => '<a href="'.$data[$i].'">'.$header[$i].'</a>');
         } else if ( $types[$i] == "img" ) {
-          $properties = $properties + array($header[$i] => '<img src="'.$data[$i].'"/>');
+          $properties = $properties + array($header[$i] => '<img src="'.$data[$i].'" target="_blank"/>');
         } else {
           $properties = $properties + array($header[$i] => $data[$i]);
         }
